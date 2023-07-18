273622171335410264edd5f0709948a1c96197b7
Aranjin Enkhbat <62941804+aranjinsuren@users.noreply.github.com>
2023-07-17 20:27:38 -0400

    217. Contains Duplicate HashSet Solution Added Runtime - O(n)

--- src/Easy/ContainsDuplicate.java
+++ src/Easy/ContainsDuplicate.java
@@ -5,14 +5,17 @@ import java.util.Set;
 
 public class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
+        // Create a set to store the numbers
         Set<Integer> set = new HashSet<Integer>();
         boolean temp;
         for (int num : nums){
             temp = set.add(num);
+            // If temp is false, it means that the number is already in the set
             if (!temp){
                 return true;
             }
         }
+        // If the loop ends, it means that there are no duplicates
         return false;
     }
 }
